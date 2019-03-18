9b55448f2557d44ee205417b21328d6997ab4326
Martín Biagini <martin.biagini@redmondsoftware.com>
2019-03-18 14:51:49 -0300

    package structure defined

---
+++ src/main/java/ohh1/logic/Ohh1Problem.java
@@ -0,0 +1,29 @@
+package ohh1.logic;
+
+import java.util.List;
+
+import gps.api.Problem;
+import gps.api.Rule;
+import gps.api.State;
+
+public class Ohh1Problem implements Problem {
+
+	@Override
+	public State getInitState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isGoal(State state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Rule> getRules() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
